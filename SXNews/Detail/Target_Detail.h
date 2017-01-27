fb75a9f89cb2c8e13e1a1540cc91ad01230e5059
wangshiyu13 <wangshiyu13@163.com>
2017-01-28 01:10:59 +0800

    Search Modularization

---
+++ SXNews/Detail/Target_Detail.h
@@ -0,0 +1,13 @@
+//
+//  Target_Detail.h
+//  SXNews
+//
+//  Created by wangshiyu13 on 2017/1/27.
+//  Copyright © 2017年 ShangxianDante. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Target_Detail : NSObject
+
+@end
---
+++ SXNews/Detail/Target_Detail.m
@@ -0,0 +1,13 @@
+//
+//  Target_Detail.m
+//  SXNews
+//
+//  Created by wangshiyu13 on 2017/1/27.
+//  Copyright © 2017年 ShangxianDante. All rights reserved.
+//
+
+#import "Target_Detail.h"
+
+@implementation Target_Detail
+
+@end
